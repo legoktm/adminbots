5ed70c1b7876e063be1c76dcdabf0e6c2bba5467
Kunal Mehta <legoktm@gmail.com>
2013-06-04 18:17:47 -0700

    fix syntax error

--- localsettings.py
+++ localsettings.py
@@ -9,7 +9,7 @@ def update(config):
     config['ns_username'] = 'legobot'
     config['ns_pw'] = private.ns_pw
     config['authenticate'] = True
-    config['memory']['aiv'] = '600M',
+    config['memory']['aiv'] = '600M'
     config['memory']['ufaa'] = '256M'
     config['memory']['rfpp'] = '256M'
     return config
