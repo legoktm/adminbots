b1b2950e4f231b2f7ec32edab4e5ce968ecb33f1
Kunal Mehta <legoktm@gmail.com>
2013-06-08 17:02:27 -0700

    new main.py script to load all the modules and run the bot

---
+++ main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+"""
+Copyright (C) 2013 Legoktm
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+"""
+
+import private
+from mtirc import bot
+from mtirc import settings
+
+import ufaa
+import aiv
+
+config = settings.config
+
+config['nick'] = 'leeeeeeeegobot'
+config['connections']['card.freenode.net']['channels'] = ['##legoktm-bots', '##legoktm-bots-chatter']
+config['connections']['irc.wikimedia.org'] = {'channels': ['#en.wikipedia',
+                                                           ],
+                                              'authenticate': False,
+                                              }
+config['default_channel'] = '##legoktm-bots'
+config['modules']['ufaa'] = ufaa.run
+config['modules']['aiv'] = aiv.run
+config['authenticate'] = True
+config['ns_username'] = 'legobot'
+config['ns_pw'] = private.ns_pw
+
+b = bot.Bot(config)
+b.cache.set('uaa', [])
+b.cache.set('aiv', [])
+b.run()
