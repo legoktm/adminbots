d8b4973e44224f88053faa2eeb0ded5cb91b4f51
Kunal Mehta <legoktm@gmail.com>
2013-09-08 00:51:54 -0700

    Revert "Allow for getting count of all log entries"
    This reverts commit cd39b2e8ca20df5b0fffeecf4e89e972ddd635dd.

--- action_count.py
+++ action_count.py
@@ -12,11 +12,9 @@ def on_msg(**kw):
         return True
     sp = text.split(' ')
     if len(sp) < 3:
-        kw['bot'].queue_msg(kw['channel'], '!(a|l) (log_action|"all") username[@dbname]')
+        kw['bot'].queue_msg(kw['channel'], '!(a|l) log_action username[@dbname]')
     type_ = sp[0][1]
     action = sp[1]
-    if action == 'all':
-        type_ = 'all'
     username = sp[2]
     if '@' in username:
         dbname = username.split('@', 1)[1]
@@ -31,25 +29,16 @@ JOIN user
 ON user_id=log_user
 WHERE """
     if type_ == 'a':
+        human = 'log actions'
         query += 'log_action = ?'
-    elif type_ == 'l':
-        query += 'log_type = ?'
-
-    if type_ == 'all':
-        data = (username,)
     else:
-        data = (username, action)
-
+        human = 'log entries'
+        query += 'log_type = ?'
     query += '\nAND user_name = ?;'
 
     with db.connect(dbname) as cur:
-        cur.execute(query, data)
+        cur.execute(query, (action, username))
         count = cur.fetchone()[0]
-
-    if type_ == 'a':
-        human = 'log actions'
-    else:
-        human = 'log entries'
 
     kw['bot'].queue_msg(kw['channel'], '{0}@{1} has {2} {3} of type {4}'.format(
         username, dbname, count, human, action
