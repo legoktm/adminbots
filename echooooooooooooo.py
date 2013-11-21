e0d7cd9c33d5d645f825832c82bb82c6bfe844d2
Kunal Mehta <legoktm@gmail.com>
2013-11-20 17:48:19 -0800

    Get notifications on multiple sites

--- echooooooooooooo.py
+++ echooooooooooooo.py
@@ -12,6 +12,12 @@ channel = '##legoktm'
 INIT = False
 LOCAL_CACHE = {}
 USERS = [u'Legobot', u'Legoktm']
+SITES = [
+    ('en', 'wikipedia'),
+    ('mediawiki', 'mediawiki'),
+    ('wikidata', 'wikidata'),
+    ('meta', 'meta')
+]
 LAST = 0
 
 def init(**kw):
@@ -32,25 +38,29 @@ def on_msg(**kw):
         return True
     LAST = time.time()
     for username in USERS:
-        site = pywikibot.Site('en', 'wikipedia', username)
-        site.login()
-        print username
-        print 'Logged in as: {0}'.format(site.username())
-        req = api.Request(site=site,
-                          action='query',
-                          meta='notifications',
-                          notprop='list|count')
-        data = req.submit()
-        count = int(data['query']['notifications']['count'])
-        print count
-        if not (username in LOCAL_CACHE):
-            LOCAL_CACHE[username] = -10
-        if LOCAL_CACHE[username] < count or force:
-            print 'queue msg'
-            kw['bot'].queue_msg(channel, '{0}: You have {1} unread notifications '
-                                         '(https://en.wikipedia.org/wiki/Special:Notifications)'.format(username, count))
+        for langproj in SITES:
+            site = pywikibot.Site(langproj[0], langproj[1], username)
+            site.login()
+            print username
+            print 'Logged in as: {0}'.format(site.username())
+            req = api.Request(site=site,
+                              action='query',
+                              meta='notifications',
+                              notprop='list|count')
+            data = req.submit()
+            count = int(data['query']['notifications']['count'])
+            print count
+            if not (langproj in LOCAL_CACHE):
+                LOCAL_CACHE[langproj] = {}
+            if not (username in LOCAL_CACHE[langproj]):
+                LOCAL_CACHE[langproj][username] = -10
+            if LOCAL_CACHE[langproj][username] < count or force:
+                print 'queue msg'
+                link = 'https://' + site.hostname() + site.nice_get_address('Special:Notifications')
+                kw['bot'].queue_msg(channel, '{0}@{1}: You have {2} unread notifications '
+                                             '({3})'.format(username, site.dbName(), count, link))
 
-        LOCAL_CACHE[username] = count
+            LOCAL_CACHE[username] = count
     return True
 
 hooks.add_hook('on_msg', 'echo', on_msg)
